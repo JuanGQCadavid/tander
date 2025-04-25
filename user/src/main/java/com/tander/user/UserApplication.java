fda8467d9dce575ae43065d1160924f45bd1917b
Andre Ahuna <anahun@taltech.ee>
2025-04-25 15:07:44 +0300

    Configure postgres, vscode logging

--- user/src/main/java/com/tander/user/UserApplication.java
+++ user/src/main/java/com/tander/user/UserApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserApplication {
 
     public static void main(String[] args) {
+        DatabaseInitializer.initialize("userservice_db");
         SpringApplication.run(UserApplication.class, args);
     }
 

1ca5f5d5b823a13a7150fe4035ab7d5d9a44a6a4
Andre Ahuna <anahun@taltech.ee>
2025-05-01 18:22:27 +0300

    Init profile service

---
+++ profile/src/test/java/com/tander/profile/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tander.profile.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

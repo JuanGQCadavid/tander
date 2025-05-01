bce8644c2fbf60f577b2140e3dfa3f1e91eba932
Juan Gonzalo Quıroz Cadavıd <jquirozcadavid@gmail.com>
2025-05-01 16:00:36 +0300

    Adding initialize projects

---
+++ chatmanagment/src/main/java/com/tander/chatmanagment/ChatmanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.tander.chatmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatmanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatmanagmentApplication.class, args);
+	}
+
+}
---
+++ chatmanagment/src/test/java/com/tander/chatmanagment/ChatmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tander.chatmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

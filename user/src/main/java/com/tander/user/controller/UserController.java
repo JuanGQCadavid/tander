6ba813872d6c95c58f49cd5e4ec7ed29fd9e11b3
Andre Ahuna <anahun@taltech.ee>
2025-05-07 15:56:09 +0300

    Login page + cors

--- user/src/main/java/com/tander/user/controller/UserController.java
+++ user/src/main/java/com/tander/user/controller/UserController.java
@@ -13,6 +13,7 @@ import com.tander.user.dto.UserDto;
 import com.tander.user.dto.UserRegistrationDto;
 import com.tander.user.service.UserService;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@CrossOrigin(origins = "http://localhost:8081/")
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
